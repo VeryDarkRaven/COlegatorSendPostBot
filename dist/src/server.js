c9a1cd723eb051e5848d17cdd5e62723d4a8646c
Raven <olegpolny2002@gmail.com>
2024-04-16 14:20:49 +0300

    add command build-and-start

--- dist/src/server.js
+++ dist/src/server.js
@@ -9,6 +9,7 @@ function expressStart() {
     const app = (0, express_1.default)();
     const host = "0.0.0.0";
     const port = 3000;
+    // const port = Math.floor(2000 + Math.random() * (9000 + 1 - 2000));
     app.get("/", (req, res) => {
         res.send("Hello World!");
     });
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ function expressStart(): void {
   const app = express();
   const host = "0.0.0.0";
   const port = 3000;
+  // const port = Math.floor(2000 + Math.random() * (9000 + 1 - 2000));
 
   app.get("/", (req, res) => {
     res.send("Hello World!");
